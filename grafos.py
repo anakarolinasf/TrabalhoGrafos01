204ef4cabc5630f7e99cb65dde7143262739c8f2
Ana Karolina <anakarolinas2014@gmail.com>
2026-02-24 08:41:41 -0300

    Adiciona arquivo inicial de grafos

---
+++ grafos.py
@@ -0,0 +1,13 @@
+estados = [
+    "AC","AL","AP","AM","BA","CE","DF","ES","GO","MA",
+    "MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN",
+    "RS","RO","RR","SC","SP","SE","TO"
+]
+arestas = [
+    ("CE", "PI"),
+    ("CE", "PE"),
+    ("CE", "PB"),
+    ("CE", "RN"),
+    ("SP", "MG"),
+    ("SP", "RJ"),
+]
